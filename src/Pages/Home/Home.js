193a15472c1c0b96f15e7c347a9d75390114edc6
ashwin <telmoreashwin2001@gmail.com>
2023-05-24 15:23:00 +0530

    final

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -241,7 +241,7 @@ export default function Home() {
 
         <footer className="p-4  bg-gray-800 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
           <div className="sm:flex sm:items-center sm:justify-between space-y-12 w-3/4 sm:px-4 mx-auto">
-            <Link to={'/notfound'} target="_blank" className="flex items-center mb-4 sm:mb-0">
+            <Link to={'#'} target="_blank" className="flex items-center mb-4 sm:mb-0">
               <img src={logogg} className="mr-4 h-8" alt="Flowbite Logo" />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">EzyRidez</span>
             </Link>
@@ -264,7 +264,7 @@ export default function Home() {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023
-            <a href="/" target="_blank" className="hover:underline">EzyRidez</a>. All Rights Reserved.
+            <a href="#" target="_blank" className="hover:underline">EzyRidez</a>. All rights Reserved.
           </span>
         </footer>
       </div>
